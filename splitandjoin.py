12e4cc7a2b6ac4f8f78133a18d6692717aaa515d
Akshay Patel <akshayblevel@gmail.com>
2024-01-17 17:26:05 +0530

    String split and join

---
+++ splitandjoin.py
@@ -0,0 +1,11 @@
+# split the string with space and join again with dash (-)
+
+def split_and_join(line):
+    a = line.split(" ")
+    a = "-".join(a)
+    return a
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
