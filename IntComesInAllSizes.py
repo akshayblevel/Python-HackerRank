5f5cb69621d8fb2ccafeabd44740ee1df9f6ca58
Akshay Patel <akshayblevel@gmail.com>
2024-03-15 19:48:57 +0530

    Integer Comes in All Sizes

---
+++ IntComesInAllSizes.py
@@ -0,0 +1,19 @@
+# Print the result of a power b plus c power d on one line.
+
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+print(pow(a, b) + pow(c, d))
+
+'''
+INPUT
+9
+29
+7
+27
+
+OUTPUT
+4710194409608608369201743232  
+'''
